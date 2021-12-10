26609db9a1079f362cd5ba51f0b290527968a837
Osher David <osherdavid111@gmail.com>
2021-12-10 16:02:26 +0200

    Changed the basic navigation view

--- app/src/main/java/com/eph/tor/ui/direct_messages/DirectMessagesFragment.java
+++ app/src/main/java/com/eph/tor/ui/direct_messages/DirectMessagesFragment.java
@@ -1,4 +1,4 @@
-package com.eph.tor.ui.notifications;
+package com.eph.tor.ui.direct_messages;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.eph.tor.R;
 
-public class NotificationsFragment extends Fragment {
+public class DirectMessagesFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private DirectMessagesViewModel directMessagesViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        directMessagesViewModel =
+                ViewModelProviders.of(this).get(DirectMessagesViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_direct_messages, container, false);
+        final TextView textView = root.findViewById(R.id.text_direct_messages);
+        directMessagesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
